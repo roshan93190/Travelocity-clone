eb1048da30ebb0eeabcf7a1681582225dc32a379
Roshan <codingninja1111@gmail.com>
2022-05-07 17:26:13 +0530

    Adding all files

---
+++ components/navbar.js
@@ -0,0 +1,24 @@
+function navbar(){
+    return `
+    
+    
+    <div class="Navstart">
+    <div id="Navimg">
+        <div>
+            <img src="https://www.travelocity.com/_dms/header/logo.svg?locale=en_US&siteid=80001&2" alt="">
+        </div>
+    
+    </div>
+    <div id="Navsignin">
+        <div><a href="">Español</a></div>
+        <div style="width:150px ;"><a href="">List your property</a></div>
+        <div><a href="">Support</a></div>
+        <div><a href="">Trips</a></div>
+        <div><a href="signin.html">Sign in</a></div>
+    </div>
+         </div>
+    
+    `
+}
+
+export default navbar
---
+++ details.js
@@ -0,0 +1,347 @@
+let arr=[
+    {
+        img:"https://images.trvl-media.com/hotels/4000000/3860000/3851700/3851663/958ad657.jpg?impolicy=resizecrop&rw=455&ra=fit",
+        title:"The Leela Palace New Delhi",
+        city:"New Delhi",
+refundable:"Fully refundable",
+reserve:"Reserve now, pay later",
+starRating:"4.8/5",
+rupees:"203",
+total:"240"
+
+    },
+  
+
+
+
+// 257 total
+
+    {img:"https://images.trvl-media.com/hotels/2000000/1310000/1304400/1304393/370d2f58.jpg?impolicy=resizecrop&rw=455&ra=fit",
+        title:"Shangri-La Eros, New Delhi",
+        city:"Connaught Place",
+refundable:"Fully refundable",
+reserve:"Reserve now, pay later",
+starRating:"4.5/5",
+rupees:"100",
+total:"118"
+
+    },
+
+    {img:"https://images.trvl-media.com/hotels/1000000/470000/465100/465005/13a4747e.jpg?impolicy=resizecrop&rw=455&ra=fit",
+        title:"  Taj Palace, New Delhi",
+        city:"New Delhi",
+refundable:"Fully refundable",
+reserve:"Reserve now, pay later",
+starRating:"4.5/5",
+rupees:"218",
+total:"257"
+
+    },
+
+    {img:"https://images.trvl-media.com/hotels/2000000/1530000/1528800/1528788/e1cfe61e.jpg?impolicy=resizecrop&rw=455&ra=fit",
+        
+    title:"Radisson Blu Plaza Delhi Airport",
+        city:"South West",
+refundable:"Fully refundable",
+reserve:"Reserve now, pay later",
+starRating:"4.1/5",
+rupees:"98",
+total:"104",
+    
+    },
+  
+// 
+
+// More
+
+    {img:"https://images.trvl-media.com/hotels/11000000/10220000/10210200/10210101/532d599e.jpg?impolicy=resizecrop&rw=455&ra=fit",
+    title:"Roseate House",
+        city:"Aerocity",
+refundable:"Fully refundable",
+reserve:"Reserve now, pay later",
+starRating:"4.2/5",
+rupees:"153",
+total:"181"
+    },
+   
+    {img:"https://images.trvl-media.com/hotels/77000000/76950000/76945300/76945266/eafc5354.jpg?impolicy=resizecrop&rw=455&ra=fit",
+        title:" Hotel Stanley by Lord Grand",
+        city:"Mahilpalpur",
+refundable:"Fully refundable",
+reserve:"Reserve now, pay later",
+starRating:"3.5/5",
+rupees:"39",
+total:"50"
+
+    },
+
+    {img:"https://images.trvl-media.com/hotels/21000000/20170000/20160800/20160759/e31ef8a9.jpg?impolicy=resizecrop&rw=455&ra=fit",
+        title:"  The Oberoi, New Delhi",
+        city:"New Delhi",
+refundable:"Fully refundable",
+reserve:"Reserve now, pay later",
+starRating:"4.9/5",
+rupees:"204",
+total:"240"
+
+    },
+
+
+    {img:"https://images.trvl-media.com/hotels/1000000/540000/536600/536519/2525421d.jpg?impolicy=resizecrop&rw=455&ra=fit",
+        title:" The Metropolitan Hotel and Spa New Delhi",
+        city:"Connaught Place",
+refundable:"Fully refundable",
+reserve:"Reserve now, pay later",
+starRating:"4.1/5",
+rupees:"73",
+total:"82"
+
+    },
+
+
+    {img:"https://images.trvl-media.com/hotels/9000000/8740000/8738300/8738241/9fdc0ad2.jpg?impolicy=resizecrop&rw=455&ra=fit",
+        title:"Vivanta New Delhi,Dwarka",
+        city:"Dwarka",
+refundable:"Fully refundable",
+reserve:"Reserve now, pay later",
+starRating:"4.6/5",
+rupees:"279",
+total:"329"
+
+    },
+    {img:"https://images.trvl-media.com/hotels/16000000/15240000/15233900/15233833/b02869f0.jpg?impolicy=resizecrop&rw=455&ra=fit",
+        title:"Haveli Dharampura",
+        city:"New Delhi",
+refundable:"Fully refundable",
+reserve:"Reserve now, pay later",
+starRating:"4.4/5",
+rupees:"125",
+total:"147"
+
+    },
+    {img:"https://images.trvl-media.com/hotels/14000000/13090000/13083600/13083538/380e3879.jpg?impolicy=resizecrop&rw=455&ra=fit",
+        title:"Pride Plaza Hotel Aerocity",
+        city:"Aerocity",
+refundable:"Fully refundable",
+reserve:"Reserve now, pay later",
+starRating:"3.8/5",
+rupees:"96",
+total:"108"
+
+    },
+    {img:"https://images.trvl-media.com/hotels/2000000/1060000/1052500/1052412/33090d68.jpg?impolicy=resizecrop&rw=455&ra=fit",
+        title:"The Roseate New Delhi",
+        city:"South West",
+refundable:"Fully refundable",
+reserve:"Reserve now, pay later",
+starRating:"4.2/5",
+rupees:"175",
+total:"206"
+
+    },
+
+    {img:"https://images.trvl-media.com/hotels/5000000/4660000/4651800/4651727/c9be0802.jpg?impolicy=resizecrop&rw=455&ra=fit",
+        title:"Welcomhotel by ITC Hotels, Dwarka, New Delhi",
+        city:"Dwarka",
+refundable:"Fully refundable",
+reserve:"Reserve now, pay later",
+starRating:"3.7/5",
+rupees:"76",
+total:"85"
+
+    },
+    {img:"https://images.trvl-media.com/hotels/6000000/5310000/5309200/5309112/b1b35867.jpg?impolicy=resizecrop&rw=455&ra=fit",
+        title:"The Leela Ambience Convention Hotel Delhi",
+        city:"New Delhi",
+refundable:"Fully refundable",
+reserve:"Reserve now, pay later",
+starRating:"4.4/5",
+rupees:"151",
+total:"178"
+
+    },
+    {img:"https://images.trvl-media.com/hotels/14000000/13240000/13235700/13235605/91d05645.jpg?impolicy=resizecrop&rw=455&ra=fit",
+        title:"Andaz Delhi - a concept by Hyatt",
+        city:"South West",
+refundable:"Fully refundable",
+reserve:"Reserve now, pay later",
+starRating:"4.5/5",
+rupees:"151",
+total:"178"
+
+    },
+    {img:"https://images.trvl-media.com/hotels/1000000/530000/521100/521096/13c1553d.jpg?impolicy=resizecrop&rw=455&ra=fit",
+        title:"The Grand New Delhi",
+        city:"Vasant Kunj",
+refundable:"Fully refundable",
+reserve:"Reserve now, pay later",
+starRating:"3.6/5",
+rupees:"118",
+total:"139"
+
+    },
+    {img:"https://images.trvl-media.com/hotels/10000000/9100000/9093700/9093615/34de1401.jpg?impolicy=resizecrop&rw=455&ra=fit",
+        title:"Hyatt Place Gurgaon Udyog Vihar",
+        city:"Gurugram",
+refundable:"Fully refundable",
+reserve:"Reserve now, pay later",
+starRating:"4.2/5",
+rupees:"203",
+total:"240"
+
+    },
+    {img:"https://images.trvl-media.com/hotels/1000000/530000/526400/526368/6f11d5a0.jpg?impolicy=resizecrop&rw=455&ra=fit",
+        title:"Ambassador, New Delhi - IHCL SeleQtions",
+        city:"New Delhi",
+refundable:"Fully refundable",
+reserve:"Reserve now, pay later",
+starRating:"4.2/5",
+rupees:"125",
+total:"147"
+
+    },
+    {img:"https://images.trvl-media.com/hotels/39000000/38680000/38678300/38678265/77d4580c.jpg?impolicy=resizecrop&rw=455&ra=fit",
+        title:"Kelvish Hotel",
+        city:"Mahipalpur",
+refundable:"Fully refundable",
+reserve:"Reserve now, pay later",
+starRating:"4.8/5",
+rupees:"38",
+total:"43"
+
+    },
+    {img:"https://images.trvl-media.com/hotels/3000000/2280000/2270400/2270398/5a1ec55a.jpg?impolicy=resizecrop&rw=455&ra=fit",
+        title:"The Leela Ambience Gurugram Hotel & Residences",
+        city:"Rajokari",
+refundable:"Fully refundable",
+reserve:"Reserve now, pay later",
+starRating:"4.8/5",
+rupees:"203",
+total:"240"
+
+    },
+    {img:"https://images.trvl-media.com/hotels/10000000/9150000/9141400/9141328/b50c24b2.jpg?impolicy=resizecrop&rw=455&ra=fit",
+        title:"Novotel New Delhi Aercity Hotel",
+        city:"Aerocity",
+refundable:"Fully refundable",
+reserve:"Reserve now, pay later",
+starRating:"4.0/5",
+rupees:"98",
+total:"203"
+
+    },
+    {img:"https://images.trvl-media.com/hotels/5000000/4330000/4327800/4327701/8258bc4d.jpg?impolicy=resizecrop&rw=455&ra=fit",
+        title:"The Oberoi Gurgaon",
+        city:"Gurugram",
+refundable:"Fully refundable",
+reserve:"Reserve now, pay later",
+starRating:"4.8/5",
+rupees:"203",
+total:"240"
+
+    }];
+// array of object over
+
+// let sorting=document.getElementById("sorting");
+// sorting.addEventListener("change",sortingPrice);
+
+// // appendData(arr);
+// window.addEventListener("load",function(){
+// appendData(arr);
+// })
+
+// function sortingPrice(){
+
+// let value=document.getElementById("sorting").value;
+// if(value==="price"){
+//     arr.sort(function(a,b){
+//         return Number(a.total)-Number(b.total);
+//     })    
+//     
+// }
+
+// }
+
+appendData(arr);  
+
+let data=JSON.parse(localStorage.getItem("information"));
+// console.log(data);
+
+
+function appendData(arr){
+
+    // document.getElementById("dataAppender").innerHTML=null;
+arr.map(function(ele){
+   
+    let mainDiv=document.createElement("div");
+    let obj={
+        img:ele.img,
+        title:ele.title,
+        city:ele.city,
+        refundable:ele.refundable,
+        reserve:ele.reserve,
+        starRating:ele.starRating,
+        rupees:ele.rupees,
+        total:ele.total
+    };
+    mainDiv.addEventListener("click",(function(ele){
+        
+        
+        localStorage.setItem("information",JSON.stringify(obj));
+window.location.href="checkout.html";
+    }))
+   mainDiv.style.display="flex";
+   mainDiv.setAttribute("id","mainDiv");
+   mainDiv.style.cursor="pointer"
+let div1=document.createElement("div");
+div1.setAttribute("id","div1");
+// div1.style.border="1px solid black";
+let img=document.createElement("img");
+img.src=ele.img;
+div1.append(img);
+
+
+let div2=document.createElement("div");
+div2.setAttribute("id","div2"
+)
+// div2.style.border="2px solid black"
+let title=document.createElement("h2");
+title.setAttribute("id","title");
+title.style.fontWeight="bold"
+title.innerText=ele.title;
+let city=document.createElement("p");
+city.innerText=ele.city;
+city.style.fontSize="15px";
+city.style.fontWeight="20px";
+
+let refundable=document.createElement("p");
+refundable.innerText=ele.refundable;
+// refundable.setAttribute("class","marginTop")
+ refundable.style.color="green";
+let reserve=document.createElement("p");
+reserve.innerText=ele.reserve;
+reserve.setAttribute("class","marginTop")
+let starRating=document.createElement("p");
+starRating.innerText=`${ele.starRating} le bhaiye ompho`;
+
+let amount=document.createElement("h2");
+amount.innerText=`$${ele.rupees}`;
+amount.setAttribute("class","marginLeft");
+amount.setAttribute("id","amount");
+
+let total=document.createElement("p");
+total.innerText=`$${ele.total}       total`;
+total.setAttribute("class","marginLeft");
+total.style.fontWeight="500"
+total.style.fontSize="11px"
+
+div2.append( title,city,refundable,reserve,amount,total)
+mainDiv.append(div1,div2);
+document.getElementById("dataAppender").append(mainDiv);
+})
+document.getElementById("search").addEventListener("click",function(){
+    window.location.reload()
+})
+    
+}
+// appendData
